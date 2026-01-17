182748ef09fb143d54c6c8fad4d176abc31f216e
Johnathon D Smith <jd49490@me.com>
2026-01-17 14:05:19 -0500

    <leader>ff include gitignore

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,4 @@
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file explorer" })
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help" })
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,19 @@ return {
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>ff', function()
+        builtin.find_files({
+          hidden = true,
+          no_ignore = true,
+        })
+      end, {})
+      vim.keymap.set('n', '<leader>fg', function()
+        builtin.live_grep({
+          additional_args = function()
+            return { "--hidden", "--no-ignore", "--glob", "!.git/*" }
+          end,
+        })
+      end, {})
     end
   },
   {
